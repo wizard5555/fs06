c01837e57def26cbbc5e56e2c431e1f8c03c63e0
Daniel Patricio Lima <patricio.wizard@gmail.com>
2022-12-19 19:10:51 -0300

    I've changed README

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
 - NodeJS v18.12.1
 - Express v4.18
 - MySQL/MariaDB v8
+
+## Instalação
+Para baixar a aplicação execute o seguinte comando em seu terminal:
+`git clone https://github.com/wizard5555/fs06.git`
+
+Após baixar a aplicação, entre no diretório através do terminal e execute:
+`npm install' ou 'npm i`
+
+## Configuração do BD
+É necessário configurar a credencial do Banco de Dados
+
+### Estrutura do Banco de Dados
+Acesse seu cliente de terminal do seu Banco de Dados;
+
+> Copie e execute os comandos que estao no arquivo `/db.sql`;
--- db.sql
+++ db.sql
@@ -1,44 +1,16 @@
--- Para listar os bancos de dados de um servidor --
-SHOW DATABASES;
-
 -- Para criar um banco de dados --
-CREATE DATABASE fs06_teste;
-
--- Para excluir um banco de dados --
-DROP DATABASE fs06_teste;
-
+CREATE DATABASE fs06_api;
 
 -- Para selecionar um banco de dados --
-USE fs06_teste;
+USE fs06_api;
 
--- Para listar as tabelas de um banco de dados --
-SHOW TABLES;
-
--- Para criar uma tabela --
-CREATE TABLE tb_teste (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(20) NOT NULL,
-    email VARCHAR(40) NOT NULL
-);
-
--- Para detalhar a estrutura de uma tabela --
-DESC tb_teste;
-
--- Para deletar uma tabela --
-DROP TABLE tb_teste;
-
-
-
+-- Para criar tabela --
 CREATE TABLE tb_veiculo (
     id INT(11) PRIMARY KEY AUTO_INCREMENT,
     marca VARCHAR(20) NOT NULL,
     modelo VARCHAR(20) NOT NULL,
     ano INT(4) NOT NULL
 );
-
--- Para alterar uma coluna --
-ALTER TABLE tb_veiculo MODIFY ano INT(5) NOT NULL;
-
 
 -- Manipulação de Dados --
 
@@ -65,35 +37,6 @@ INSERT INTO tb_veiculo (ano, modelo, marca)
 VALUES (2021, 'Mobi', 'Fiat');
 
 
--- Para buscar dados na tabela --
-SELECT * FROM tb_veiculo;
-
--- * vai selecionar todas as colunas --
-
-SELECT marca, modelo FROM tb_veiculo;
-
-SELECT modelo as Model, id as Identifier FROM tb_veiculo;
-
-
--- filtrando dados de uma tabela --
-SELECT * FROM tb_veiculo WHERE marca='Chevrolet';
-
-SELECT * FROM tb_veiculo WHERE ano >= 2014;
-
-SELECT * FROM tb_veiculo WHERE marca='Chevrolet' OR marca='Fiat';
-
-SELECT * FROM tb_veiculo WHERE ano BETWEEN 2014 AND 2022;
-
-
--- ordenar pelo ano
-SELECT * FROM tb_veiculo   ORDER BY ano ;
-
--- filtrar todos os modelos que começam com C
-SELECT * FROM tb_veiculo WHERE modelo LIKE 'c%';
-
-
-
-
 CREATE TABLE tb_cliente (
     id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
@@ -108,15 +51,3 @@ VALUES
 ('Michael', 'ale@email.com', '32312312312'),
 ('Victor', 'ale@email.com', '42312312312'),
 ('Guilherme', 'ale@email.com', '52312312312');
-
-
-
--- Para excluir dados de uma tabela --
-DELETE FROM tb_cliente WHERE id=5;
-
--- Para editar dados de uma tabela --
-UPDATE tb_cliente SET nome = 'Guilherme da Silva' WHERE id=5;
-
-
-SELECT * FROM movies WHERE legenda LIKE '%I am happy%'
-
